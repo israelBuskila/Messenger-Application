5c3cfc88c3b8d2eccd19ed04d9c252e2d0c4b9a1
IsraelBuskila <israelad3@gmail.com>
2021-05-18 07:50:30 +0300

    delete clg

--- server/routes/chats.js
+++ server/routes/chats.js
@@ -6,7 +6,7 @@ const groupsDAL = require("../DAL/groupsDAL");
 const usersLoginDAL = require("../DAL/usersLoginDAL");
 
 router.post("/", async (req, res, next) => {
-  console.log(req.headers["x-access-token"]);
+ 
   let token = req.headers["x-access-token"];
   if (!token) {
     return res.status(401).send({ auth: false, message: "No token provided." });
